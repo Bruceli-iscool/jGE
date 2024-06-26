93918904b1251b6c76e6488d4341d2f987a42001
Bruce Li <isanoob1897@outlook.com>
2024-06-26 00:45:21 +0000

    add

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String args[]) {
+        Window n = new Window("", 500, 600);
+        n.addText("John is very very cool", 50, 50);
+    }
+}
--- src/Window.java
+++ src/Window.java
@@ -1,12 +1,36 @@
 import javax.swing.*;
-import java.awt.event.*;
 
 
 public class Window {
-    protected JFrame f;
+    private final JFrame f;
+
+    public Window() {
+        f = new JFrame();
+        f.setVisible(true);
+    }
+    public Window(String windowTitle) {
+        f = new JFrame(windowTitle);
+        f.setVisible(true);
+    } 
     public Window(String windowTitle, int xSize, int ySize) {
         f = new JFrame(windowTitle);
         f.setVisible(true);
         f.setSize(xSize, ySize);
+    } 
+    public void setSize(int xSize, int ySize) {
+        f.setSize(xSize, ySize);
+    } 
+    public void name(String name) {
+        f.setTitle(name);
+    } 
+    public void addText(String text, int xPosition, int yPosition) {
+        JLabel n = new JLabel(text);
+        n.setHorizontalAlignment(xPosition);
+        n.setVerticalAlignment(yPosition);
+        f.add(n);
+        f.repaint();
+    }
+    public JFrame getWindow() {
+        return f;
     }
 }
